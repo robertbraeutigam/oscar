149b6106597ea740e69d53e418a13cf131167809
Robert Braeutigam <robert@mathema.de>
2022-05-26 20:42:21 +0200

    Revert "Switch to JUnit, tests still do not work"
    This reverts commit 64d2ae6f1d2ea6e62cf420f91c79ad311d4ba346.

--- language/src/test/java/com/vanillasource/oscar/IntLiteralNodeTests.java
+++ language/src/test/java/com/vanillasource/oscar/IntLiteralNodeTests.java
@@ -1,15 +1,15 @@
 package com.vanillasource.oscar;
 
+import org.testng.annotations.Test;
 import com.oracle.truffle.api.Truffle;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.testng.Assert.*;
 
+@Test
 public final class IntLiteralNodeTests {
-   @Test
    public void testReturnsTheLiteral() {
       int result = (int) execute(new IntLiteralNode(12));
 
-      assertEquals(12, result);
+      assertEquals(result, 12);
    }
 
    private Object execute(IntLiteralNode node) {
